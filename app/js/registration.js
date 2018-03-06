5ef4fe69d4c104ae3b72e65a35b8b426a10276d5
Andreas Lymalm <andreas.lymalm@gmail.com>
2018-03-06 16:01:57 +0100

    created login and logout functions

--- app/js/registration.js
+++ app/js/registration.js
@@ -1,21 +1,32 @@
-
+$(() => logOut());
 
 ui.regSubmitBtn.on("click", function(e) {
     e.preventDefault();
-    console.log(ui.regEmail.val());
-    // Create user with e-mail and password
-    firebase.auth().createUserWithEmailAndPassword(ui.regEmail.val(), ui.regPassword.val()).catch(function (error) {
-        console.log("Something went wrong with the registration");
-    });
 
-    var messageListRef = firebase.database().ref("all-users/" + ui.regUsername.val()).set({
-        "avatar": 'default',
-        "e-mail": ui.regEmail.val(),
-        "logged-in": true,
-        "name": ui.regFullname.val(),
-        "theme": 0,
-    });
-    var path = messageListRef.toString();
-    // path will be something like
-    // 'https://sample-app.firebaseio.com/message_list/-IKo28nwJLH0Nc5XeFmj'
+    // Create user with e-mail and password
+    firebase.auth().createUserWithEmailAndPassword(ui.regEmail.val(), ui.regPassword.val())
+        .then(() => {
+            var messageListRef = firebase.database().ref("all-users/" + ui.regUsername.val()).set({
+                "avatar": 'default',
+                "e-mail": ui.regEmail.val(),
+                "logged-in": true,
+                "name": ui.regFullname.val(),
+                "theme": 0,
+            });
+            return console.log("Inloggad!");
+        })
+        .catch(function (error) {console.log("Something went wrong with the registration");});
+
 });
+
+function logIn(){
+    firebase.auth().signInWithEmailAndPassword("a.lymalm@gmail.com", "kanelbulle")
+        .then(() => console.log("Du är inloggad"))
+        .catch(() => console.log("Kunde inte logga in :("));
+}
+
+function logOut(){
+    firebase.auth().signOut()
+        .then(() => console.log("Du är utloggad"))
+        .catch(() => console.log("Kunde inte logga ut :("));
+}
