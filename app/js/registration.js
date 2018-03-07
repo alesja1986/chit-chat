a92975a486423d59747122aa2b91ce9bd6974045
Andreas Lymalm <andreas.lymalm@gmail.com>
2018-03-07 11:48:12 +0100

    Minor adjustment to placeholders + adjustments to database con nection

--- app/js/registration.js
+++ app/js/registration.js
@@ -1,4 +1,8 @@
-$(() => logOut());
+
+// Check if user is logged in. If so, redirect to that page
+if(firebase.auth().currentUser){
+    window.location.replace("../chat.html");
+}
 
 ui.regSubmitBtn.on("click", function(e) {
     e.preventDefault();
@@ -6,28 +10,16 @@ ui.regSubmitBtn.on("click", function(e) {
     // Create user with e-mail and password
     firebase.auth().createUserWithEmailAndPassword(ui.regEmail.val(), ui.regPassword.val())
         .then(() => {
-            var messageListRef = firebase.database().ref("all-users/" + ui.regUsername.val()).set({
+            firebase.database().ref("all-users/" + ui.regUsername.val()).set({
                 "avatar": 'default',
                 "e-mail": ui.regEmail.val(),
                 "logged-in": true,
                 "name": ui.regFullname.val(),
                 "theme": 0,
-            });
-            return console.log("Inloggad!");
+            })
+            .then(() => window.location.replace("../chat.html"));
+            return;
         })
-        .catch(function (error) {console.log("Something went wrong with the registration");});
+        .catch(error => console.log("Something went wrong with the registration\n" + error));
 
 });
-
-function logIn(){
-    firebase.auth().signInWithEmailAndPassword("a.lymalm@gmail.com", "kanelbulle")
-        .then(() => console.log("Du är inloggad"))
-        .catch(() => console.log("Kunde inte logga in :("));
-}
-
-function logOut(){
-    firebase.auth().signOut()
-        .then(() => console.log("Du är utloggad"))
-        .catch(() => console.log("Kunde inte logga ut :("));
-}
-
