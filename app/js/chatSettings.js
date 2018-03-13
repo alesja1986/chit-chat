343640c86f522178b915211c48637e41db8cb6fd
alesja1986 <alesja.rudaja@gmail.com>
2018-03-13 20:20:42 +0100

    chat-settings layout

---
+++ app/js/chatSettings.js
@@ -0,0 +1,12 @@
+ui.openChatSettingsBtn.click(function(){
+      $("#chat-settings").show();
+      ui.closeChatSettingsBtn.show();
+
+});
+
+ui.closeChatSettingsBtn.click(function(){
+    ui.closeChatSettingsBtn.hide();
+    $("#chat-settings").hide();
+
+});
+
