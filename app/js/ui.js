96f19d338e59373758d6521312a6440db4042d70
alesja1986 <alesja.rudaja@gmail.com>
2018-03-13 08:51:35 +0100

    chat-window layout done

--- app/js/ui.js
+++ app/js/ui.js
@@ -5,4 +5,9 @@ const ui = {
     regEmail: $("#e-mail"),
     regPassword: $("#password"),
     regSubmitBtn: $("#registration-btn"),
+
+    //chat window
+    sendMessageBtn: $("#send-message"),
+    chatMessageInput:$('#chat-message-input'),
+    chatSettingsBtn: $("#chat-settings-btn"),
 }
