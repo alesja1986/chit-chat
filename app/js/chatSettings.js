26e1abcd1a341305600afaf2a745da4ea6b10c1d
alesja1986 <alesja.rudaja@gmail.com>
2018-03-18 22:36:07 +0100

    bug with chatname

--- app/js/chatSettings.js
+++ app/js/chatSettings.js
@@ -1,3 +1,5 @@
+let sendNewTheme;
+
 ui.openChatSettingsBtn.click(function(){
     // Hide chat view
     ui.chatWindow.hide();
@@ -5,15 +7,43 @@ ui.openChatSettingsBtn.click(function(){
     // Show chat settings
     ui.chatSettings.show();
     ui.closeChatSettingsBtn.show();
+    showThisChatName();
 });
 
-ui.closeChatSettingsBtn.click(function(){
+ui.closeChatSettingsBtn.click(function() {
     // Hide chat settings
     ui.chatSettings.hide();
     ui.closeChatSettingsBtn.hide();
 
     // Show chat view
     ui.chatWindow.show();
-});
+    });
 
 
+
+function showThisChatName() {
+    ui.chatNameInput = firebase.database().ref("chatrooms/" + activeChat + "/settings").once('value', snapshot => {
+        ui.chatNameInput.val(snapshot.val().name);
+    });
+}
+
+//save clicked img name
+    $('.chat-theme img').on("click", function () {
+        let newTheme = this.src;
+        let newThemeLength = newTheme.length;
+        sendNewTheme = newTheme.substring(newThemeLength - 7, newThemeLength - 4);
+        return sendNewTheme
+    });
+
+    //send new name and theme to db
+    ui.createChatRoomBtn.on("click", function () {
+        let newChatName = ui.chatNameInput.val();
+        firebase.database().ref("chatrooms/" + activeChat + "/settings").update({
+            "name": newChatName,
+            "theme": sendNewTheme
+        })
+    })
+
+    ui.deleteChatRoomBtn.on("click", function () {
+
+    });
